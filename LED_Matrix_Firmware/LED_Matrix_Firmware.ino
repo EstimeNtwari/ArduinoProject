24e255834fe68acfdca7e715e720c73290bef1b0
EstimeNtwari <estimentwari@gmail.com>
2015-11-11 15:28:20 -0500

    Added .gitignore and Adafruit Libraries

---
+++ LED_Matrix_Firmware/LED_Matrix_Firmware.ino
@@ -0,0 +1,13 @@
+#include <Adafruit_GFX.h>
+#include <Wire.h>
+#include <Adafruit_LEDBackpack.h>
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
