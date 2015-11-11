376527aab1347cfeb133f93add262fe2af39dcf4
EstimeNtwari <estimentwari@gmail.com>
2015-11-11 16:49:24 -0500

    Added instructions to files

--- LED_Matrix_Firmware/LED_Matrix_Firmware.ino
+++ LED_Matrix_Firmware/LED_Matrix_Firmware.ino
@@ -1,12 +1,35 @@
+//----------------------HOWTO----------------------------------------
+/*  
+    matrix.displaybuffer[i] : directly access display buffer. 
+    matrix.drawBitmap(0, 0,  mybitmap, 16, 32, LED_ON); outputs "mybmp" to buffer
+    matrix.drawLine(0,0, 7,15, Mat1); 
+
+  //INITIALLIZING BUFFER
+    static const uint16_t PROGMEM
+      mybitmap[] =
+      { B0011110000111100,
+        B0100001000111100,
+        B1010010100111100,
+        B1000000100111100,
+        B1010010100111100,
+        B1001100100111100,
+        B0100001000111100,
+        B0011110000111100 };
+  
+  
+   write the changes we just made to the display. display buffer to LED. 
+   matrix.writeDisplay();
+  */
+
+//-----------------------ENDHOWTO---------------------------------------
+
+
 #include <Adafruit_GFX.h>
 #include <Wire.h>
 #include <Adafruit_LEDBackpack.h>
 
-#ifndef _BV
-  #define _BV(bit) (1<<(bit))
-#endif
 
-Adafruit_LEDBackpack matrix = Adafruit_LEDBackpack();
+Adafruit_8x16matrix matrix = Adafruit_8x16matrix();  //specify your display buffer
 
 uint8_t counter = 0;
 
@@ -20,11 +43,5 @@ void setup() {
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
-  // write the changes we just made to the display
-  matrix.writeDisplay();
-  
-
-
 
 }
