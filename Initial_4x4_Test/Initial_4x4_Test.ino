7af7101d6150b3d8599dfb1edb3bd24b25be22df
EstimeNtwari <estimentwari@gmail.com>
2015-11-12 22:10:55 -0500

    Added Bitmap

--- Initial_4x4_Test/Initial_4x4_Test.ino
+++ Initial_4x4_Test/Initial_4x4_Test.ino
@@ -23,6 +23,16 @@ void loop() {
 
   // paint one LED per row. The HT16K33 internal memory looks like
   // a 4x4 bit matrix (4 rows, 4 columns)
+  static const uint16_t PROGMEM
+      mybitmap[] =
+      { B0011110000111100,
+        B0100001000111100,
+        B1010010100111100,
+        B1000000100111100,
+        B1010010100111100,
+        B1001100100111100,
+        B0100001000111100,
+        B0011110000111100 };
   
   for (uint8_t i=0; i<4; i++) {
     // draw a diagonal row of pixels
