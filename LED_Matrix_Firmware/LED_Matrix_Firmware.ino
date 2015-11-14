e78ec566060d2d28669dcdcaf15f3ca72b56d03d
EstimeNtwari <estimentwari@gmail.com>
2015-11-14 17:45:01 -0500

    Added Function Protypes and Serial Input

--- LED_Matrix_Firmware/LED_Matrix_Firmware.ino
+++ LED_Matrix_Firmware/LED_Matrix_Firmware.ino
@@ -1,6 +1,7 @@
 //----------------------HOWTO----------------------------------------
 /*  
-    matrix.displaybuffer[i] : directly access display buffer. 
+    matrix.displaybuffer[i] : directly access display buffer. check if node has been hit.
+    matrix.drawPixel() : draw to a pixel.
     matrix.drawBitmap(0, 0,  mybitmap, 16, 32, LED_ON); outputs "mybmp" to buffer
     matrix.drawLine(0,0, 7,15, Mat1); 
 
@@ -31,17 +32,65 @@
 
 Adafruit_8x16matrix matrix = Adafruit_8x16matrix();  //specify your display buffer
 
-uint8_t counter = 0;
+int dart_counter;
+int player; //potential multiplayer support.
 
 void setup() {
   // put your setup code here, to run once:
 
   Serial.begin(9600);
-  matrix.begin(0x70); 
+  matrix.begin(0x70); //go to first matrix
+  dart_count=0; 
 
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  if(dart_count<=2){ //
+
+    //Detect data coming in from Kinect. This could be enclosed in a "ready" conditional.
+    if(Serial.available()){
+      int sengment= Serial.read();  //retreive location of incoming dart. set 0 for miss
+    }
+    switch (segment){
+
+      //case i
+      //matrix.drawPixel(i,j, LED_ON); //write to segment i
+      //enumerate segments. and write to buffer.
+
+    }
+
+    //write buffer to Dartboard LEDs. 
+    matrix.writeDisplay();
+    }else{
+      //clear Dartboard
+      matrix.clear();
+      
+    }
+    
 
 }
+
+//Custom Function Listing.
+
+//Function to map segment to LED.
+void PaintSegment(int seg){
+
+}
+
+//Function to check if segment has already been hit. 
+bool CheckSegment(int seg){
+
+}
+
+//Subroutine to indicate to the user that they can throw dart. i.e Light up all LED 2 times. etc.
+void ShowReady(){
+
+}
+
+//Function to animate clearing of screen. 
+void AnimateClear(){
+
+}
+
+
